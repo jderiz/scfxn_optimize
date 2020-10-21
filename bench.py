0eeb0706c1dd75caa89f47ff5ba2da24adee0448
qpxr <qpxr.vr@gmail.com>
2020-10-21 11:12:34 +0200

    prep merge master

--- bench.py
+++ bench.py
@@ -2,13 +2,9 @@ import pickle
 import sys
 import time
 import os
-import matplotlib.pyplot as plt
-
 from skopt import forest_minimize, gp_minimize, gbrt_minimize, callbacks
-from skopt.plots import (plot_convergence, plot_evaluations, plot_objective)
-
-from design import Designer, design_with_config
-from hyperparams import scfxn_ref15_space
+from design import design_with_config
+from hyperparams import scfxn_ref15_space, ref15_weights
 
 if __name__ == "__main__":
     """
@@ -19,20 +15,21 @@ if __name__ == "__main__":
 
     # instantiate result array and specific number calls to objective per optimizer
     res = []
-    n_calls = 200  # Objective Function evaluations
+    n_calls = 100  # Objective Function evaluations
     start_time = time.time()  # overall Runtime measuring
 
     dimensions = scfxn_ref15_space
- 
-    designer = Designer(filename='benchmark/1K9P_A_relax_0001.pdb')
-    objective = design_with_config
 
+    objective = design_with_config
+    default_parameters = [val for k, val in ref15_weights]
     # setup callbacks for logging
     timer_callback = callbacks.TimerCallback()
     forest_check = callbacks.CheckpointSaver('.forest_checkpoints.gz')
     gbrt_check = callbacks.CheckpointSaver('.gbrt_checkpoints.gz')
     gp_check = callbacks.CheckpointSaver('.gp_checkpoints.gz')
     optimizer = sys.argv[1]
+    xi = 0.001
+    kappa = 0.1
     print(
         '_________start optimize________' +
         '_____________{}________________'.format(optimizer)
@@ -42,42 +39,46 @@ if __name__ == "__main__":
         forest_result = forest_minimize(
             func=objective,
             dimensions=dimensions,
-            acq_func='EI',  # expected Improvement (evaluate whats best to use)
+            acq_func='PI',  # expected Improvement (evaluate whats best to use)
             n_calls=n_calls,
-            # x0=default_parameters,
+            x0=default_parameters,
             n_jobs=-1,
+            xi=xi,
+            kappa=kappa,
             callback=[timer_callback, forest_check]
         )  # All availiable Cores if aquisition =lbfgs
-        with open("results/res_{}_{}.pkl".format('forest', str(n_calls)), "wb") as file:
+        with open("results/res_{}_{}_similar.pkl".format('forest', str(n_calls)), "wb") as file:
             pickle.dump(forest_result, file)
     if optimizer in ['gbrt', 'all']:
         print('GradientBoostedTrees')
         gbrt_res = gbrt_minimize(
             func=objective,
             dimensions=dimensions,
-            acq_func='EI',
+            acq_func='PI',
             n_calls=n_calls,
+            x0=default_parameters,
             n_jobs=-1,
+            xi=xi,
+            kappa=kappa,
             callback=[timer_callback, gbrt_check]
         )
-        with open("results/res_{}_{}.pkl".format('gbrt', str(n_calls)), "wb") as file:
+        with open("results/res_{}_{}_similar.pkl".format('gbrt', str(n_calls)), "wb") as file:
             pickle.dump(gbrt_res, file)
     if optimizer in ['gp', 'all']:
         print('Gaussian Processes')
         gp_res = gp_minimize(
             func=objective,
             dimensions=dimensions,
-            acq_func="EI",
+            acq_func="PI",
             n_calls=n_calls,
             n_jobs=-1,
+            x0=default_parameters,
+            xi=xi,
+            kappa=kappa,
             callback=[timer_callback, gp_check]
         )
-        with open("results/res_{}_{}.pkl".format('gp', str(n_calls)), "wb") as file:
+        with open("results/res_{}_{}_similar.pkl".format('gp', str(n_calls)), "wb") as file:
             pickle.dump(gp_res, file)
-
-    # plot_convergence(*res)
-    # plt.show()
-    # plt.savefig('results/convergence.png', dpi=300, bbox_inches="tight")
 
     took = time.time() - start_time
     print("Took: {} to run".format(time.strftime("%H: %M: %S", time.gmtime(took))))
--- design.py
+++ design.py
@@ -6,6 +6,8 @@ from pyrosetta import *
 import create_scfxn
 from skopt.utils import use_named_args
 from hyperparams import scfxn_ref15_space
+from Bio.Align import substitution_matrices
+from Bio import pairwise2
 
 prs.init("-ex1 "
          "-ex2 "
@@ -21,10 +23,11 @@ def design_with_config(**config):
     print("RUN: ", run)
 
     print(config)
-    ref15 = get_fa_scorefxn()
-    scfxn = create_scfxn.creat_scfxn_from_config(config=config)
+    ref15 = get_fa_scorefxn()  # REF15 score function with default weights for loss calulation
+    scfxn = create_scfxn.creat_scfxn_from_config(config=config)  # optimization score Function
     pose = pose_from_pdb('benchmark/1K9P_A_relax_0001.pdb')  # easiest struct for optimizer picking
-    starting_pose = Pose()
+    native_pose = Pose()
+    native_pose.assign(pose)
     resfile = "./design.resfile"
     with open(resfile, "w") as f:
         f.write("ALLAAxc \n")
@@ -33,86 +36,16 @@ def design_with_config(**config):
     taskf = pyrosetta.rosetta.core.pack.task.TaskFactory()
     taskf.push_back(pyrosetta.rosetta.core.pack.task.operation.InitializeFromCommandline())
     taskf.push_back(pyrosetta.rosetta.core.pack.task.operation.ReadResfile(resfile))
-    # taskf.push_back(operation.RestrictToRepacking())
     packer = pyrosetta.rosetta.protocols.minimization_packing.PackRotamersMover(scfxn)
     packer.task_factory(taskf)
-    # print(taskf.create_task_and_apply_taskoperations(pose))
+    taskf.create_task_and_apply_taskoperations(pose)
     packer.apply(pose)
 
     print("Optimized scfxn score: ", scfxn(pose))
     print('REF15 Score ', ref15(pose))
-    # we optimize the Designing function but eval on standard ref15
-    return ref15(pose)
+    # TODO: What defines our loss, for now use REF15
+    bloss62 = substitution_matrices.load("BLOSUM62")
+    similar = pairwise2.align.globaldx(pose.sequence(), native_pose.sequence(), bloss62, score_only=True)
+    print('similarity ::', similar)
+    return similar
 
-
-class Designer(object):
-    def __init__(self, filename=None, scorefxn=None):
-        """Instatiate Designer"""
-        if filename:
-            self.pose = pose_from_pdb(filename=filename)
-        self.starting_pose = Pose()
-        self.starting_pose.assign(self.pose)
-        self.scorefxn = get_fa_scorefxn()
-
-        if scorefxn:
-            self.scorefxn_G = scorefxn
-            print("running with custom scorefxn_G")
-        else:
-            self.scorefxn_G = get_fa_scorefxn()
-            print("running with REF15 scorefxn_G")
-
-        # self.relax = prs.rosetta.protocols.relax.FastRelax()
-
-    def fixbb(self, scfxn=None):
-
-        """
-            Performs the RosettaDesign protocol to change a structure's
-            amino acid sequence while maintaining a FIXED backbone.
-            Generates the structure.pdb file.
-            """
-        if scfxn:
-            self.scorefxn_G = scfxn
-
-        resfile = "./design.resfile"
-        with open(resfile, "w") as f:
-            f.write("ALLAAxc \n")
-            f.write("start\n")
-        # resfile = prs.rosetta.core.pack.task.operation.ReadResfile("./design.resfile")
-        # task = prs.rosetta.core.pack.task.TaskFactory()
-        # task.push_back(resfile)
-
-        # job = PyJobDistributor("fixbb", 20, self.scorefxn)
-        # job.native_pose = self.starting_pose
-        #
-        # while not job.job_complete:
-        #     self.pose.assign(self.starting_pose)
-        #     fixbb.apply(self.pose)
-        #     #self.relax.apply(self.pose)
-        #     fixbb.output_decoy(self.pose)
-
-        score_fxn = self.scorefxn_G
-        pose = self.pose
-        taskf = pyrosetta.rosetta.core.pack.task.TaskFactory()
-        taskf.push_back(pyrosetta.rosetta.core.pack.task.operation.InitializeFromCommandline())
-        taskf.push_back(pyrosetta.rosetta.core.pack.task.operation.ReadResfile(resfile))
-        # taskf.push_back(operation.RestrictToRepacking())
-        packer = pyrosetta.rosetta.protocols.minimization_packing.PackRotamersMover(score_fxn)
-        packer.task_factory(taskf)
-        print(taskf.create_task_and_apply_taskoperations(pose))
-        packer.apply(pose)
-
-        print(self.scorefxn_G(pose))
-        return self.scorefxn_G(pose)
-
-    @use_named_args(dimensions=scfxn_ref15_space)
-    @staticmethod
-    def test(**config):
-        print(config)
-        print(create_scfxn.creat_scfxn_from_config(config=config))
-        return random.randint(0, 100)
-
-
-if __name__ == '__main__':
-    prs.init("-ex1 -ex2")
-    designer = Designer('/home/iwe7/meiler/scfxn_optimize/benchmark/1K9P_A_relax_0001.pdb')
-    designer.fixbb()
