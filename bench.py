26fa4ccc709a5e72d6455b754c751136e5ae7dfc
qpxr <qpxr.vr@gmail.com>
2020-11-12 15:15:02 +0100

    fix format bench.py

--- bench.py
+++ bench.py
@@ -10,12 +10,12 @@ from multiprocessing import (Pipe, Pool, active_children, cpu_count,
 
 from joblib import Parallel, delayed
 from numpy import repeat
-from skopt import (Optimizer, callbacks, forest_minimize, gbrt_minimize,
-                   gp_minimize)
 
 from design import design_with_config
 from hyperparams import ref15_weights, scfxn_ref15_space
 from setup import parallel_configs, runs_per_config
+from skopt import (Optimizer, callbacks, forest_minimize, gbrt_minimize,
+                   gp_minimize)
 
 logger = multiprocessing.log_to_stderr()
 logger.setLevel(logging.INFO)
@@ -45,24 +45,25 @@ if __name__ == "__main__":
     # "GP, GBRT, ET, RF"
     estimator = sys.argv[1]
     loss_value = sys.argv[2]
+
     if not loss_value:
-	print('ERROR: No loss value defined as second argument')
-	exit() 
-   # more exploit then explore
+        print('ERROR: No loss value defined as second argument')
+        exit() 
+    # more exploit then explore
     xi = 0.001
     kappa = 0.1
     acq_func_kwargs = {"xi": xi, "kappa": kappa}
     print(
-        "_________start optimize________"
-        + "_____________{}________________".format(estimator)
-    )
+       "_________start optimize________"
+       + "_____________{}________________".format(estimator)
+       )
 
     optimizer = Optimizer(
-        dimensions=dimensions,
-        base_estimator=estimator,
-        n_jobs=-1,
-        acq_func_kwargs=acq_func_kwargs,
-    )
+       dimensions=dimensions,
+       base_estimator=estimator,
+       n_jobs=-1,
+       acq_func_kwargs=acq_func_kwargs,
+       )
 
     # MAIN optimiaztion Loop with custom result handling
     # using all available cores with Pool()
@@ -78,7 +79,7 @@ if __name__ == "__main__":
         pending_jobs = cpu_count()
         jobs = []
 
-        def wait_and_exit():
+        def save_and_exit():
             print("wait for all Processes to finish and close pool")
             # TODO: be certain all jobs are finished
             _DONE = True
@@ -103,21 +104,21 @@ if __name__ == "__main__":
                     jobs_for_current_config = 0
                 # instantiate job knowing which config it belongs to.
                 job = tp.apply_async(
-                    objective,
-                    (config,),
-                    callback=partial(_callback, config=config),
-                    error_callback=error_callback,
-                )
+                        objective,
+                        (config,),
+                        callback=partial(_callback, config=config),
+                        error_callback=error_callback,
+                        )
                 # Append map_result to jobs list
                 jobs.append(job)
                 calls += 1
             else:
-                # wait for all processes to finish then exit Pool
-                pending_jobs -= 1
-                print('PENDING: ', pending_jobs)
+              # wait for all processes to finish then exit Pool
+              pending_jobs -= 1
+              print('PENDING: ', pending_jobs)
 
-                if pending_jobs == 0:
-                    wait_and_exit()
+            if pending_jobs == 0:
+                save_and_exit()
 
         def _callback(map_res, config=None):
             """
@@ -133,8 +134,10 @@ if __name__ == "__main__":
 
             make_process()
             # Check if key exists
+
             if c_hash in result_buffer.keys():
                 # if this is the last run for config 
+
                 if len(result_buffer[c_hash]) == (runs_per_config - 1):
                     print('SAVE RESULT')
                     # if so, compute mean over results for config and tell optimizer
@@ -142,11 +145,11 @@ if __name__ == "__main__":
                     # print(result_buffer[c_hash])
                     res = result_buffer[c_hash]
                     _res = {
-                        "bloss62": sum([x["bloss62"] for x in res]) / len(res),
-                        "ref15": sum([x["ref15"] for x in res]) / len(res),
-                        "scfxn": sum([x["scfxn"] for x in res]) / len(res),
-                        "weights": config,
-                    }
+                            "bloss62": sum([x["bloss62"] for x in res]) / len(res),
+                            "ref15": sum([x["ref15"] for x in res]) / len(res),
+                            "scfxn": sum([x["scfxn"] for x in res]) / len(res),
+                            "weights": config,
+                            }
                     results.append(_res)
                     opti_res = optimizer.tell(config, _res[loss_value])
                     del result_buffer[c_hash]  # dont need that buffer
@@ -164,18 +167,18 @@ if __name__ == "__main__":
         # TODO: check for rounding
         initial_batches = int(cpu_count() / runs_per_config)
         print(
-            "Starting ",
-            initial_batches,
-            "initial_configs each running ",
-            runs_per_config,
-        )
+           "Starting ",
+           initial_batches,
+           "initial_configs each running ",
+           runs_per_config,
+           )
         initial_map_results = []
         prev_config = None
 
         # Start Loop
 
         for _ in range(cpu_count()):
-            # map runs_per_config processes with same config
+        # map runs_per_config processes with same config
             make_process()
 
         # while there are jobs in the list being processed wait for them to finish
@@ -186,29 +189,32 @@ if __name__ == "__main__":
                 print('NO MORE ACTIVE CHILDREN')
                 # when no more child processes sleep a minute to let things settle
                 # then terminate Pool
-                time.sleep(60)
                 tp.terminate()
+                time.sleep(60)
+
                 break
 
-            for async_result in jobs:
-                if async_result.ready():
-                    jobs.remove(async_result)
+        for async_result in jobs:
+            if async_result.ready():
+                jobs.remove(async_result)
+
+    ##########################
     # result printing and saving
     took = time.time() - start_time
     print("Took for ALL: {} to run".format(time.strftime("%H: %M: %S", time.gmtime(took))))
     # save custom results
     with open(
         "results/{}_{}_res_{}_{}.pkl".format(
-            time.strftime("%H_%M"), estimator, str(n_calls), loss_value
-        ),
+           time.strftime("%H_%M"), estimator, str(n_calls), loss_value
+           ),
         "wb",
-    ) as file:
+         ) as file:
         pickle.dump(results, file)
     # save results from optimizer.tell()
     with open(
-            "results/{}_{}_res_{}_{}_optimizer.pkl".format(
-            time.strftime("%H_%M"), estimator, str(n_calls), loss_value
-        ),
+        "results/{}_{}_res_{}_{}_optimizer.pkl".format(
+           time.strftime("%H_%M"), estimator, str(n_calls), loss_value
+           ),
         "wb",
-    ) as file:
+        ) as file:
         pickle.dump(optimizer_results, file)
