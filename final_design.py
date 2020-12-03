e210ede7e33290f54a915e4ed57a0a5f9c8ac2ab
qpxr <qpxr.vr@gmail.com>
2020-12-03 16:39:05 +0100

    add winner configs "all_sm"

--- final_design.py
+++ final_design.py
@@ -9,22 +9,20 @@ from joblib import Parallel, delayed
 import pyrosetta as prs
 from design import design_with_config
 
-prs.init(
-options="-ex1 -ex2", set_logging_handler=True, extra_options="-mute basic -mute core -mute protocols -linmem_ig 10 -archive_on_disk /tmp/rosetta"
-)  # no output from the design process
+# prs.init(
+# options="-ex1 -ex2", set_logging_handler=True, extra_options="-mute basic -mute core -mute protocols -linmem_ig 10 -archive_on_disk /tmp/rosetta"
+# )  # no output from the design process
 
-def fdesign(config):
-    """TODO: Docstring for fdesign.
+if __name__ == "__main__":
+    with open('all_sm.pkl', 'rb') as h:
+        all_sm = pickle.load(h)
 
-    :config: TODO
-    :returns: TODO
-
-    """
+    config = all_sm.mean().values
 
     with get_context('spawn').Pool(processes=cpu_count(), maxtasksperchild=3) as tp:
         # make 200 processes     
 
-        result_set = tp.map(design_with_config, [config for i in range(200)] ) 
+        result_set = tp.map(design_with_config, [config for i in range(1000)] ) 
         with open('results/final_results.pkl') as h:
             res = pickle.load(h)
             res.append(pd.DataFrame(result_set))
