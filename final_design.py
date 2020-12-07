c30f780fde546d67f1bd255ec9d5ff860eceaaaa
je535nypy <je535nypy@galaxy101.sc.uni-leipzig.de>
2020-12-07 09:18:43 +0100

    wip

--- final_design.py
+++ final_design.py
@@ -9,9 +9,9 @@ from joblib import Parallel, delayed
 import pyrosetta as prs
 from design import design_with_config
 
-# prs.init(
-# options="-ex1 -ex2", set_logging_handler=True, extra_options="-mute basic -mute core -mute protocols -linmem_ig 10 -archive_on_disk /tmp/rosetta"
-# )  # no output from the design process
+prs.init(
+options="-ex1 -ex2", set_logging_handler=True, extra_options="-mute basic -mute core -mute protocols -linmem_ig 10 -archive_on_disk /tmp/rosetta"
+)  # no output from the design process
 
 if __name__ == "__main__":
     with open('all_sm.pkl', 'rb') as h:
