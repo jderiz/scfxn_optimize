b83c3904c5299f56388c8df5696a10d0600c8cfe
IWE7 <iwe7@localhost.localdomain>
2020-12-14 10:56:29 +0100

    wip

--- design.py
+++ design.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import random
+import time
 
 import pyrosetta as prs
 from Bio import pairwise2
@@ -9,7 +10,7 @@ from Bio.Align import substitution_matrices
 from pyrosetta import (Pose, PyJobDistributor, get_fa_scorefxn, init,
                        pose_from_pdb)
 from skopt.utils import use_named_args
-import time
+
 import create_scfxn
 from hyperparams import scfxn_ref15_space
 from setup import runs_per_config
@@ -21,31 +22,32 @@ def initialize():
     global prs
     print('INITIALIZED')
     prs.init(
-    options="-ex1 -ex2", set_logging_handler=True, extra_options="-linmem_ig 10 -archive_on_disk /tmp/rosetta -mute core -mute"
-    )  # no output from the design process
+        options="-ex1 -ex2", set_logging_handler=True, extra_options="-linmem_ig 10 -archive_on_disk /tmp/rosetta -mute core -mute basic -mute protocols"
+    )  # no utput from the design process
     prs.logging_support.set_logging_sink()
     logger = logging.getLogger("rosetta")
     logger.setLevel(logging.ERROR)
-    logger.handlers.pop() # remove stream handler
+    logger.handlers.pop()  # remove stream handler
 
     rosetta_log_handler = logging.FileHandler('rosetta.log')
     rosetta_log_handler.setLevel(logging.DEBUG)
     logger.addHandler(rosetta_log_handler)
-    global pdbs 
+    global pdbs
     pdbs = {}
 
     # get all protein pdbs
+
     for pdb in os.listdir("benchmark"):
         if pdb.endswith(".pdb"):
             #  store actual Pose() object
             pdbs.update({pdb.split('.')[0]: pose_from_pdb("benchmark/" + pdb)})
 
-
+    # print(pdbs)
 
 
 @use_named_args(dimensions=scfxn_ref15_space)
-def design_with_config(**config):
-    start_time = time.time()  #  Runtime measuring
+def design_with_config(**config) -> dict:
+    start_time = time.time()  # Runtime measuring
     print('DESIGNING')
     ref15 = (
         get_fa_scorefxn()
@@ -53,12 +55,13 @@ def design_with_config(**config):
     scfxn = create_scfxn.creat_scfxn_from_config(
         config=config
     )  # optimization score Function
-    
+
     # pick random
     # pose = random.choice(pdbs)
-    # key = random.choice(pdbs.keys())
-    # pose = pdbs[key]
+    # prot_name = random.choice(pdbs.keys())
+    # pose = pdbs[prot_name]
     pose = pdbs['1K9P']
+    prot_name = '1K9P'
 
     # copy pose for comparison after design
     native_pose = Pose()
@@ -70,9 +73,11 @@ def design_with_config(**config):
 
     # def run(pose):
     taskf = prs.rosetta.core.pack.task.TaskFactory()
-    taskf.push_back(prs.rosetta.core.pack.task.operation.InitializeFromCommandline())
+    taskf.push_back(
+        prs.rosetta.core.pack.task.operation.InitializeFromCommandline())
     taskf.push_back(prs.rosetta.core.pack.task.operation.ReadResfile(resfile))
-    packer = prs.rosetta.protocols.minimization_packing.PackRotamersMover(scfxn)
+    packer = prs.rosetta.protocols.minimization_packing.PackRotamersMover(
+        scfxn)
     packer.task_factory(taskf)
     taskf.create_task_and_apply_taskoperations(pose)
     packer.apply(pose)
@@ -84,10 +89,16 @@ def design_with_config(**config):
     ) / len(pose.sequence())
 
     # moritz says its okay to return energy normalized by length
-    result = {"sequence": pose.sequence(), "prot_len": len(pose.sequence()), "bloss62": -similar, "ref15": (ref15(pose)/len(pose.sequence())), "scfxn": (scfxn(pose)/len(pose.sequence()))}
-    print('DESIGN_DONE: ',result)
+    result = {"sequence": pose.sequence(),
+              "prot_len": len(pose.sequence()), 
+              "prot_name": prot_name,
+              "bloss62": -similar,
+              "ref15": (ref15(pose)/len(pose.sequence())), 
+              "scfxn": (scfxn(pose)/len(pose.sequence()))}
+    print('DESIGN_DONE: ', result)
     took = time.time() - start_time
     print("Took: {} to run".format(time.strftime("%H: %M: %S", time.gmtime(took))))
 
     # This has to be serializable in order to get pickled and send back to parent
+
     return result
